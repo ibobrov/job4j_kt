1a3b9f310a117f7a594a126da4285db165992bcb
ibobrov <coderilya@gmail.com>
2023-11-29 10:46:07 +1000

    Operator FOR

---
+++ src/main/kotlin/ru/job4j/base/Printer.kt
@@ -0,0 +1,26 @@
+package ru.job4j.base
+
+fun draw(size : Int) {
+    if (size <= 0 || size % 2 == 0) {
+        println("size некорректен")
+        return
+    }
+
+    for (x in 0 until size) {
+        for (y in 0 until  size) {
+            val onDiagonal = x == y || x + y == size - 1
+            print(if (onDiagonal) "x" else " ")
+        }
+        println()
+    }
+}
+
+fun main() {
+    draw(2)
+    println()
+    draw(1)
+    println()
+    draw(3)
+    println()
+    draw(7)
+}

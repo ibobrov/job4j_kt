b91079d01fc6f4e1f6d265033567823ef3738897
ibobrov <coderilya@gmail.com>
2023-12-01 20:08:48 +1000

    Short Fun

--- src/main/kotlin/ru/job4j/base/MaxInt.kt
+++ src/main/kotlin/ru/job4j/base/MaxInt.kt
@@ -1,9 +1,5 @@
 package ru.job4j.base
 
-fun max(first: Int, second: Int) : Int {
-    return if (first > second) first else second
-}
+fun max(first: Int, second: Int) : Int = if (first > second) first else second
 
-fun max(first: Int, second: Int, third: Int) : Int {
-    return max(max(first, second), third)
-}
+fun max(first: Int, second: Int, third: Int) : Int = max(max(first, second), third)

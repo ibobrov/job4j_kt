bbaafd07324ac00b33529dd25a0c1b6bc2796ae1
ibobrov <coderilya@gmail.com>
2023-12-05 05:41:10 +1000

    Simple filter for list

---
+++ src/main/kotlin/ru/job4j/lambda/Account.kt
@@ -0,0 +1,6 @@
+package ru.job4j.lambda
+
+data class Account(val name: String, val balance : Double)
+
+val predicate = {acc : Account, name: String, isPositiveBalance : Boolean ->
+                            acc.name == name && acc.balance > 0 == isPositiveBalance}
---
+++ src/test/kotlin/ru/job4j/lambda/AccountTest.kt
@@ -0,0 +1,33 @@
+package ru.job4j.lambda
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class AccountTest {
+    private val list = listOf(
+        Account("Ivan", 1.2),
+        Account("Aleksandr", -1.0),
+        Account("Daria", 5.8),
+        Account("Daria", 0.4)
+    )
+
+    @Test
+    fun whenFilterByNameIsIvanAndBalanceNonPositive() {
+        assertThat(list.filter { predicate(it, "Ivan", false) }).isEmpty()
+    }
+
+    @Test
+    fun whenFilterByNameIsAndBalancePositive() {
+        assertThat(list.filter { predicate(it, "Ivan", true) }).containsOnly(list[0])
+    }
+
+    @Test
+    fun whenFilterByNameIsAleksandrAndBalancePositive() {
+        assertThat(list.filter { predicate(it, "Aleksandr", true) }).isEmpty()
+    }
+
+    @Test
+    fun whenFilterByNameIsDariaAndBalancePositive() {
+        assertThat(list.filter { predicate(it, "Daria", true) }).containsAll(listOf(list[2], list[3]))
+    }
+}

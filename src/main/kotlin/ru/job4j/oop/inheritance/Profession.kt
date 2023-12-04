89e4b1d445fe5241f582e1e08d9892362000b5df
ibobrov <coderilya@gmail.com>
2023-12-05 04:32:06 +1000

    Override Fun

--- src/main/kotlin/ru/job4j/oop/inheritance/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/inheritance/Profession.kt
@@ -2,18 +2,51 @@ package ru.job4j.oop.inheritance
 
 open class Profession(private val name : String) {
 
+    open fun getName() : String = name
+
+    open fun action() {
+        println("$name work")
+    }
+
     override fun toString(): String {
         return name
     }
 }
 
-class Doctor : Profession("Doctor")
+class Doctor(private val specialization: String) : Profession("Doctor") {
 
-class Engineer : Profession("Engineer")
+    override fun getName(): String {
+        return specialization
+    }
 
-class Teacher : Profession("Teacher")
+    override fun action() {
+        println("$specialization work")
+    }
+}
+
+class Engineer : Profession("Engineer") {
+
+    override fun getName(): String {
+        return "Soft ${super.getName()}"
+    }
+
+    override fun action() {
+        println("Write code")
+    }
+}
+
+class Teacher : Profession("Teacher") {
+
+    override fun action() {
+        println("Teacher checks homework")
+    }
+}
 
 fun main() {
-    val professions : List<Profession> = listOf(Doctor(), Teacher(), Engineer())
-    println(professions)
+    val professions : List<Profession> = listOf(Doctor("Aculist"), Teacher(), Engineer())
+    professions.forEach{
+        println(it.getName())
+        it.action()
+        println("")
+    }
 }

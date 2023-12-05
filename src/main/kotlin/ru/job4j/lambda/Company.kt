c1adb9da782ff99d8afe3ed33c96bf80f2211e38
ibobrov <coderilya@gmail.com>
2023-12-05 18:17:04 +1000

    Map on list

---
+++ src/main/kotlin/ru/job4j/lambda/Company.kt
@@ -0,0 +1,32 @@
+package ru.job4j.lambda
+
+import java.util.*
+
+data class Company(val name: String, val address: Address, val created: Date)
+
+data class Address(val city : String, val street : String, val house : String)
+
+fun toStringAll(companies : List<Company>) : List<String> {
+    return companies.map { "${it.name} company," +
+            " ${"${it.address.city} ${it.address.street} ${it.address.house}"}" +
+            " founded ${it.created}"}
+}
+
+fun main() {
+    val companies : List<Company> = listOf(
+        Company(
+            "Intel",
+            Address("California", "Grow street", "1"),
+            Date()
+        ),
+        Company(
+            "Microsoft",
+            Address("New York", "Central street", "1A"),
+            Date()
+        )
+    )
+
+    toStringAll(companies).forEach {
+        println(it)
+    }
+}

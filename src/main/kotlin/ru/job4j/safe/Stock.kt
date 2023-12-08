52e466f343a7e7f8eb5df7970ea65ba24cb8dc7c
ibobrov <coderilya@gmail.com>
2023-12-08 19:24:21 +1000

    Operator as

---
+++ src/main/kotlin/ru/job4j/safe/Stock.kt
@@ -0,0 +1,27 @@
+package ru.job4j.safe
+
+import java.util.Date
+
+class Stock (val name: String, val currency: String, val date: Date) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+        other as Stock
+        if (name != other.name) return false
+        if (currency != other.currency) return false
+        if (date != other.date) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + currency.hashCode()
+        result = 31 * result + date.hashCode()
+        return result
+    }
+}
